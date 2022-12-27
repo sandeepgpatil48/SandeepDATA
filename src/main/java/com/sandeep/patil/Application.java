cd9cbacf90d1bf624e2fd6816d7bd0b21e68a3ed
sandeep <sandeeppatil776@gmail.com>
2022-12-27 16:58:10 +0530

    update file

--- src/main/java/com/sandeep/patil/Application.java
+++ src/main/java/com/sandeep/patil/Application.java
@@ -2,12 +2,16 @@ package com.sandeep.patil;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		ConfigurableApplicationContext run = SpringApplication.run(Application.class, args);
+	
+		run.close();
+	
 	}
 
 }
